b72770b7a9392a48acac1133b4615ad82baab18e
Nils Refle <n.refle@gmail.com>
2025-04-23 16:01:56 +0200

    change vim diagnostic signs

--- nvim/lua/config/plugins/lspconfig.lua
+++ nvim/lua/config/plugins/lspconfig.lua
@@ -116,10 +116,10 @@ return {
       },
       signs = {
           text = {
-              [vim.diagnostic.severity.ERROR] = '',
-              [vim.diagnostic.severity.WARN] = '',
-              [vim.diagnostic.severity.INFO] = '',
-              [vim.diagnostic.severity.HINT] = '',
+              [vim.diagnostic.severity.ERROR] = '✘',
+              [vim.diagnostic.severity.WARN] = '‼',
+              [vim.diagnostic.severity.INFO] = '✦',
+              [vim.diagnostic.severity.HINT] = '➤',
           },
       },
     })
