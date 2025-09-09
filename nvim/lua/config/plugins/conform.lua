25e7d3690b701278bd1bcd664faffe74c8387c07
Nils Refle <n.refle@gmail.com>
2025-09-09 09:47:13 +0200

    Add Typescript configs

--- nvim/lua/config/plugins/conform.lua
+++ nvim/lua/config/plugins/conform.lua
@@ -1,41 +1,43 @@
 return {
-  'stevearc/conform.nvim',
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  opts = {
-    -- Define your formatters
-    formatters_by_ft = {
-      lua = { "stylua" },
-      javascript = { "prettier" },
-      rust = { "rustfmt", lsp_format = "fallback" },
-      go = { "goimports", "gofmt" },
-      php = { "php-cs-fixer" },
-      -- Use the "_" filetype to run formatters on filetypes that don't
-      -- have other formatters configured
-      ["_"] = { "trim_whitespace" }
-    },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			typescriptreact = { "prettier" },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			go = { "goimports", "gofmt" },
+			php = { "php-cs-fixer" },
+			-- Use the "_" filetype to run formatters on filetypes that don't
+			-- have other formatters configured
+			["_"] = { "trim_whitespace" },
+		},
 
-    formatters = {
-      ["php-cs-fixer"] = {
-        command = "php-cs-fixer",
-        args = {
-          "fix",
-          "--rules=@PER-CS2.0",
-          "$FILENAME",
-        },
-        stdin = false,
-      },
-    },
+		formatters = {
+			["php-cs-fixer"] = {
+				command = "php-cs-fixer",
+				args = {
+					"fix",
+					"--rules=@PER-CS2.0",
+					"$FILENAME",
+				},
+				stdin = false,
+			},
+		},
 
-    -- Set default options
-    default_format_opts = {
-      lsp_format = "fallback",
-    },
+		-- Set default options
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
 
-    -- Set up format-on-save
-    format_on_save = { timeout_ms = 500 },
-  },
-  init = function()
-    --
-  end,
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500 },
+	},
+	init = function()
+		--
+	end,
 }
