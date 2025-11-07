f7db825b7faf0102f32801ee75752b6605f3cc80
Nils Refle <n.refle@gmail.com>
2025-11-07 13:23:16 +0100

    Add shortcuts for nvim resize/navihation with hjkl

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,33 +1,45 @@
 -- Space is my leader.
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Clear search highlighting.
-vim.keymap.set('n', '<CR>', ':nohlsearch<CR><CR>')
+vim.keymap.set("n", "<CR>", ":nohlsearch<CR><CR>")
 
 -- Dont Jump on * search
-vim.keymap.set('n', '*', [[:let @/= '\<' . expand('<cword>') . '\>' <bar> set hls <cr>]])
+vim.keymap.set("n", "*", [[:let @/= '\<' . expand('<cword>') . '\>' <bar> set hls <cr>]])
 
 -- When text is wrapped, move by terminal rows, not lines, unless a count is provided.
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
 -- Paste replace visual selection without copying it.
-vim.keymap.set('v', 'p', '"_dP')
+vim.keymap.set("v", "p", '"_dP')
 
 -- Easy insertion of a trailing ; or , from insert mode.
-vim.keymap.set('i', ';;', '<Esc>A;<Esc>')
-vim.keymap.set('i', ',,', '<Esc>A,<Esc>')
-vim.keymap.set('i', '{{', '<Esc>A {}<Esc>i')
+vim.keymap.set("i", ";;", "<Esc>A;<Esc>")
+vim.keymap.set("i", ",,", "<Esc>A,<Esc>")
+vim.keymap.set("i", "{{", "<Esc>A {}<Esc>i")
 
 -- Easier window navigation
-vim.keymap.set('n', '<C-Up>', ':wincmd k<CR>')
-vim.keymap.set('n', '<C-Down>', ':wincmd j<CR>')
-vim.keymap.set('n', '<C-Left>', ':wincmd h<CR>')
-vim.keymap.set('n', '<C-Right>', ':wincmd l<CR>')
+-- Arrow
+vim.keymap.set("n", "<C-Up>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-Down>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-Left>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-Right>", ":wincmd l<CR>")
+-- hjkl
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 
 -- Easier window resize
-vim.keymap.set('n', '<S-Right>', ':vertical resize +5<CR>')
-vim.keymap.set('n', '<S-Left>', ':vertical resize -5<CR>')
-vim.keymap.set('n', '<S-Up>', ':resize -5<CR>')
-vim.keymap.set('n', '<S-Down>', ':resize +5<CR>')
+-- Arrow
+vim.keymap.set("n", "<S-Right>", ":vertical resize +5<CR>")
+vim.keymap.set("n", "<S-Left>", ":vertical resize -5<CR>")
+vim.keymap.set("n", "<S-Up>", ":resize -5<CR>")
+vim.keymap.set("n", "<S-Down>", ":resize +5<CR>")
+-- hjkl
+vim.keymap.set("n", "<A-h>", ":vertical resize +5<CR>")
+vim.keymap.set("n", "<A-l>", ":vertical resize -5<CR>")
+vim.keymap.set("n", "<A-k>", ":resize -5<CR>")
+vim.keymap.set("n", "<A-j>", ":resize +5<CR>")
